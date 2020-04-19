65eea7439011d87fba75e12ac4e892a19a0dd71a
Hyungyo Seo <51025374+hyunbridge@users.noreply.github.com>
2020-04-19 22:17:58 +0900

    시간표 파싱 안되는 오류 수정 및 기타 변경
    시간표 파싱 안되는 오류 수정, 시간표 데이터 없을 경우 사용되는 기본값 변경

--- jsonGenerator.py
+++ jsonGenerator.py
@@ -31,6 +31,12 @@ school_code = "J100005677"
 school_kind = 4  # 1 유치원, 2 초등학교, 3 중학교, 4 고등학교
 # 학급수를 정확히 입력
 num_of_classes = 9
+# 컴시간알리미 웹사이트 URL
+url = "http://comci.kr/st/"
+# 기타 옵션 입력
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+}
 
 print("Deployment Started with Configs - [%s(%s, %s, %d), Classes: %d]"
       % (school_name, school_region, school_code, school_kind, num_of_classes))
@@ -126,110 +132,167 @@ def meal():
 
 timetables = {}
 def tt():
-    # 학교명으로 검색해 학교코드 알아내기
-    search_req = urllib.request.Request(
-        'http://comci.kr:4081/98372?92744l%s' % urlparse.quote(school_name.encode("EUC-KR")),
-        data=None,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/78.0.3904.70 Safari/537.36'
-        }
-    )
+    timetables_default = {}
+    for grade in [1, 2, 3]:
+        classes = {}
+        for class_ in range(1, num_of_classes + 1):
+            classes[str(class_)] = []
+        timetables_default[str(grade)] = classes
+    try:
+        # BaseURL 알아내기
+        baseurl_req = urllib.request.Request(url, data=None, headers=headers)
+        baseurl_respns = urllib.request.urlopen(baseurl_req).read().decode('EUC-KR')
+        baseurl_pattern = re.compile("src='.*?/st'")
+        baseurl_matches = baseurl_pattern.findall(baseurl_respns)
+        if baseurl_matches:
+            base_url = baseurl_matches[0].split("'")[1][:-2]
+        else:
+            raise Exception
 
-    search_url = urllib.request.urlopen(search_req)
+        # school_ra, sc_data, 자료위치 알아내기
+        init_req = urllib.request.Request(base_url + 'st', data=None, headers=headers)
+        init_respns = urllib.request.urlopen(init_req).read().decode('EUC-KR')
+        # school_ra
+        school_ra_pattern = re.compile("url:'.?(.*?)'")
+        school_ra_matches = school_ra_pattern.findall(init_respns)
+        if school_ra_matches:
+            school_ra = school_ra_matches[0][1:]
+            school_ra_code = school_ra.split('?')[0]
+        else:
+            raise Exception
+        # sc_data
+        sc_data_pattern = re.compile("sc_data\('[0-9].*?\);")
+        sc_data_matches = sc_data_pattern.findall(init_respns)
+        if sc_data_matches:
+            sc_data = sc_data_matches[0].split("'")[1]
+        else:
+            raise Exception
+        # 일일시간표
+        daily_timetable_pattern = re.compile("일일자료=자료.*?\[")
+        daily_timetable_matches = daily_timetable_pattern.findall(init_respns)
+        if daily_timetable_matches:
+            daily_timetable = daily_timetable_matches[0].split('자료.')[1][:-1]
+        else:
+            raise Exception
+        # 원본시간표
+        original_timetable_pattern = re.compile("원자료=자료.*?\[")
+        original_timetable_matches = original_timetable_pattern.findall(init_respns)
+        if original_timetable_matches:
+            original_timetable = original_timetable_matches[0].split('자료.')[1][:-1]
+        else:
+            raise Exception
+        # 교사명
+        teachers_list_pattern = re.compile("성명=자료.*?\[th")
+        teachers_list_matches = teachers_list_pattern.findall(init_respns)
+        if teachers_list_matches:
+            teachers_list = teachers_list_matches[0].split('자료.')[1][:-3]
+        else:
+            raise Exception
+        # 과목명
+        subjects_list_pattern = re.compile('"\'>"\+자료.*?\[sb')
+        subjects_list_matches = subjects_list_pattern.findall(init_respns)
+        if subjects_list_matches:
+            subjects_list = subjects_list_matches[0].split('자료.')[1][:-3]
+        else:
+            raise Exception
 
-    # 학교 검색결과 가져오기
-    school_list = ast.literal_eval(search_url.read().decode('utf-8').replace('\x00', ''))["학교검색"]
+        # 학교명으로 검색해 학교코드 알아내기
+        search_req = urllib.request.Request(
+            base_url + school_ra + urlparse.quote(school_name.encode("EUC-KR")),
+            data=None, headers=headers
+        )
 
-    # 검색결과를 지역으로 구분하고 학교코드 가져오기
-    part_code = ""
-    for i in school_list:
-        if i[1] == school_region:
-            part_code = i[3]
-            break
+        search_url = urllib.request.urlopen(search_req)
 
-    # 이번 주 시간표와 다음 주 시간표 가져오기
-    fetch_req_1 = urllib.request.Request(
-        'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_1" % part_code, 'utf-8')).decode(
-            "utf-8"),
-        data=None,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/78.0.3904.70 Safari/537.36'
-        }
-    )
+        # 학교 검색결과 가져오기
+        school_list = ast.literal_eval(search_url.read().decode('utf-8').replace('\x00', ''))["학교검색"]
 
-    fetch_req_2 = urllib.request.Request(
-        'http://comci.kr:4081/98372?' + base64.b64encode(bytes("34739_%s_0_2" % part_code, 'utf-8')).decode(
-            "utf-8"),
-        data=None,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/78.0.3904.70 Safari/537.36'
-        }
-    )
+        # 검색결과를 지역으로 구분하고 학교코드 가져오기
+        for i in school_list:
+            if i[1] == school_region:
+                part_code = i[3]
+                break
 
-    # 시간표 디코딩
-    url_1 = urllib.request.urlopen(fetch_req_1).read().decode('utf-8').replace('\x00', '')
-    url_2 = urllib.request.urlopen(fetch_req_2).read().decode('utf-8').replace('\x00', '')
+        # 이번 주 시간표와 다음 주 시간표 가져오기
+        fetch_req_1 = urllib.request.Request(
+            base_url + school_ra_code + '?' + base64.b64encode(
+                bytes(sc_data + str(part_code) + "_0_1", 'utf-8'))
+            .decode("utf-8"),
+            data=None, headers=headers
+        )
 
-    # JSON 파싱
-    raw_0 = json.loads(url_1)
-    raw_1 = json.loads(url_2)
+        fetch_req_2 = urllib.request.Request(
+            base_url + school_ra_code + '?' + base64.b64encode(
+                bytes(sc_data + str(part_code) + "_0_2", 'utf-8'))
+            .decode("utf-8"),
+            data=None, headers=headers
+        )
 
-    date_0 = datetime.datetime.strptime(raw_0["시작일"], "%Y-%m-%d").date()
-    date_1 = datetime.datetime.strptime(raw_1["시작일"], "%Y-%m-%d").date()
-    if date_0 < date_1:
-        raw_data = [raw_0, raw_1]
-        start_date = date_0
-        end_date = date_1 + datetime.timedelta(days=7)
-    else:
-        raw_data = [raw_1, raw_0]
-        start_date = date_1
-        end_date = date_0 + datetime.timedelta(days=7)
+        # 시간표 디코딩
+        url_1 = urllib.request.urlopen(fetch_req_1).read().decode('utf-8').replace('\x00', '')
+        url_2 = urllib.request.urlopen(fetch_req_2).read().decode('utf-8').replace('\x00', '')
 
-    dates = [[], []]
-    for i in range(((start_date + datetime.timedelta(days=7)) - start_date).days):
-        date = start_date + datetime.timedelta(days=i)
-        dates[0].append(date)
-    for i in range((end_date - (end_date - datetime.timedelta(days=7))).days + 1):
-        date = start_date + datetime.timedelta(days=7 + i)
-        dates[1].append(date)
+        # JSON 파싱
+        raw_0 = json.loads(url_1)
+        raw_1 = json.loads(url_2)
 
-    for i in [0, 1]:
-        teacher_list = raw_data[i]["자료46"]  # 교사명단
-        subject_list = raw_data[i]["자료92"]  # 2글자로 축약한 과목명단 - 전체 명칭은 긴자료92에 담겨있음
-        for grade in range(1, 4):
-            for class_ in range(1, num_of_classes + 1):
-                tt = raw_data[i]["자료14"][grade][class_]  # 자료14에 각 반의 일일시간표 정보가 담겨있음
-                og_tt = raw_data[i]["자료81"][grade][class_]  # 자료81에 각 반의 원본시간표 정보가 담겨있음
-                for wday in range(6):
-                    if not dates[i][wday] in timetables:
-                        timetables[dates[i][wday]] = {}
-                    if not grade in timetables[dates[i][wday]]:
-                        timetables[dates[i][wday]][grade] = {}
-                    if not class_ in timetables[dates[i][wday]][grade]:
-                        timetables[dates[i][wday]][grade][class_] = []
-                    if wday == 6:
-                        comci_wday = 0
-                    else:
-                        comci_wday = wday + 1
-                    for day in range(len(tt[comci_wday])):
-                        if tt[comci_wday][day] != 0:
-                            subject = subject_list[int(str(tt[comci_wday][day])[-2:])]  # 뒤의 2자리는 과목명을 나타냄
-                            teacher = teacher_list[int(str(tt[comci_wday][day])[:-2])]  # 나머지 숫자는 교사명을 나타냄
-                            if not tt[comci_wday][day] == og_tt[comci_wday][day]:
-                                timetables[dates[i][wday]][grade][class_].append(
-                                    "⭐%s(%s)" % (subject, teacher))  # 시간표 변경사항 표시
-                            else:
-                                timetables[dates[i][wday]][grade][class_].append("%s(%s)" % (subject, teacher))
+        date_0 = datetime.datetime.strptime(raw_0["시작일"], "%Y-%m-%d").date()
+        date_1 = datetime.datetime.strptime(raw_1["시작일"], "%Y-%m-%d").date()
+        if date_0 < date_1:
+            raw_data = [raw_0, raw_1]
+            start_date = date_0
+            end_date = date_1 + datetime.timedelta(days=7)
+        else:
+            raw_data = [raw_1, raw_0]
+            start_date = date_1
+            end_date = date_0 + datetime.timedelta(days=7)
+
+        dates = [[], []]
+        for i in range(((start_date + datetime.timedelta(days=7)) - start_date).days):
+            date = start_date + datetime.timedelta(days=i)
+            dates[0].append(date)
+        for i in range((end_date - (end_date - datetime.timedelta(days=7))).days + 1):
+            date = start_date + datetime.timedelta(days=7 + i)
+            dates[1].append(date)
+
+        for i in [0, 1]:
+            teacher_list = raw_data[i][teachers_list]  # 교사명단
+            subject_list = raw_data[i][subjects_list]  # 2글자로 축약한 과목명단 - 전체 명칭은 긴자료92에 담겨있음
+            for grade in range(1, 4):
+                for class_ in range(1, num_of_classes + 1):
+                    tt = raw_data[i][daily_timetable][grade][class_]  # 자료14에 각 반의 일일시간표 정보가 담겨있음
+                    og_tt = raw_data[i][original_timetable][grade][class_]  # 자료81에 각 반의 원본시간표 정보가 담겨있음
+                    for wday in range(6):
+                        if not dates[i][wday] in timetables:
+                            timetables[dates[i][wday]] = {}
+                        if not grade in timetables[dates[i][wday]]:
+                            timetables[dates[i][wday]][grade] = {}
+                        if not class_ in timetables[dates[i][wday]][grade]:
+                            timetables[dates[i][wday]][grade][class_] = []
+                        if wday == 6:
+                            comci_wday = 0
+                        else:
+                            comci_wday = wday + 1
+                        for day in range(len(tt[comci_wday])):
+                            if tt[comci_wday][day] != 0:
+                                subject = subject_list[int(str(tt[comci_wday][day])[-2:])]  # 뒤의 2자리는 과목명을 나타냄
+                                teacher = teacher_list[int(str(tt[comci_wday][day])[:-2])]  # 나머지 숫자는 교사명을 나타냄
+                                if not tt[comci_wday][day] == og_tt[comci_wday][day]:
+                                    timetables[dates[i][wday]][grade][class_].append(
+                                        "⭐%s(%s)" % (subject, teacher))  # 시간표 변경사항 표시
+                                else:
+                                    timetables[dates[i][wday]][grade][class_].append("%s(%s)" % (subject, teacher))
+    except Exception as e:
+        print(e)
+
+
 
     if not today in timetables:
-        timetables[today] = None
+        timetables[today] = timetables_default
     if not yesterday in timetables:
-        timetables[yesterday] = None
+        timetables[yesterday] = timetables_default
     if not tomorrow in timetables:
-        timetables[tomorrow] = None
+        timetables[tomorrow] = timetables_default
 
 
 schdls = {}
@@ -331,5 +394,5 @@ final = {
 }
 
 with open('dist/data.json', 'w', encoding="utf-8") as make_file:
-    json.dump(final, make_file, ensure_ascii=False, indent="\t")
+    json.dump(final, make_file, ensure_ascii=False)
     print("File Created")
